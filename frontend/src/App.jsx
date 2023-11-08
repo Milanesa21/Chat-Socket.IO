b53edf44eeeb46c65b17126522863ebb2fbeb71c
Milanesa <diegojara88574@gmail.com>
2023-11-08 12:14:24 -0300

    se arreglo un problema con los Usernames

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,55 +4,100 @@ import io from 'socket.io-client'
 const socket = io("/")
 
 function App() {
-
+  const [show, setShow] = useState(true)
+  const [username, setUsername] = useState("")
   const [message, setMessage] = useState("")
   const [messages, setMessages] = useState([])
+  const [typing, setTyping] = useState("")
 
-  const handleSubmit = (e) => {
+  const handleUsernameSubmit = (e) => {
     e.preventDefault()
-    const newMessage = {
-      body:message,
-      from: 'Yo'
+    if (username.trim() !== "") {
+      socket.emit('username', username)
+      setShow(false)
+    } else { 
+      alert("Por favor, ingrese un nombre de usuario válido.")
     }
-    setMessages([...messages, newMessage])
-    socket.emit('message', message)
-  };
+  }
+
+  const handleMessageSubmit = (e) => {
+    e.preventDefault()
+    if (username.trim() !== "") {
+      const newMessage = {
+        body:message,
+        from: username
+      }
+      setMessages([...messages, newMessage])
+      socket.emit('message', newMessage)
+      setMessage("")
+    } else {
+      alert("Por favor, ingrese un nombre de usuario válido antes de enviar un mensaje.")
+    }
+  }
+
+  const handleTyping = () => {
+    socket.emit('typing', username)
+  }
 
   useEffect (() => {
-    socket.on('message', receiveMessage);
+    socket.on('messages', (messages) => {
+      setMessages(messages)
+    })
+
+    socket.on('message', receiveMessage)
+
+    socket.on('typing', (username) => {
+      setTyping(username + " está escribiendo...")
+      setTimeout(() => { setTyping("") }, 3000)
+    })
 
     return () => {
-      socket.off('message', receiveMessage);
-
+      socket.off('messages')
+      socket.off('message', receiveMessage)
+      socket.off('typing')
     }
-  }, []);
+  }, [])
 
   const receiveMessage = message => 
-  setMessages((state) => [...state, message]);
+  setMessages((state) => [...state, message])
 
   return (
     <div className ="h-screen bg-zinc-800 text-white flex items-center justify-center">
       
-      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
-        <h1 className="text-2xl font-bold my-2"> Chat </h1>
-        <input type="text" 
-        placeholder="Escriba su mensaje"
-        className="border-2 border-zinc-500 p-2 w-full text-black"
-        onChange={(e) => setMessage(e.target.value)}
-        />
+      {show  ? (
+        <form onSubmit={handleUsernameSubmit} className="bg-zinc-900 p-10">
+          <h1 className="text-2xl font-bold my-2"> Ingrese su nombre de usuario </h1>
+          <input type="text" 
+          placeholder="Escriba su nombre de usuario"
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          onChange={(e) => setUsername(e.target.value)}
+          />
+        </form>
+      ) : (
+        <form onSubmit={handleMessageSubmit} className="bg-zinc-900 p-10">
+          <h1 className="text-2xl font-bold my-2"> Chat </h1>
+          <input type="text" 
+          placeholder="Escriba su mensaje"
+          className="border-2 border-zinc-500 p-2 w-full text-black"
+          onChange={(e) => { setMessage(e.target.value); handleTyping(); }}
+          value={message}
+          />
 
-        <ul>
-       {
-        messages.map((message, i )=>(
-          <li key = {i} className= {`my-2 p-2 table rounded-md ${message.from === 'Yo' ? 'bg-purple-500'  : 'bg-zinc-700 ml-auto'}`}>
-            <span className="text-xs block">{message.from}:</span>
-        {message.body}
-            </li>
-        ))
-       }
-      </ul>
+          <p>{typing}</p>
 
-      </form>
+          <ul>
+          {
+            messages.map((message, i )=>(
+              <li key = {i} className= {`my-2 p-2 table rounded-md ${message.from === username ? 'bg-purple-500'  : 'bg-zinc-700 ml-auto'}`}>
+                <span className="text-xs block">{message.from}:</span>
+            {message.body}
+                </li>
+            ))
+          }
+          </ul>
+
+        </form>
+      )}
      
     </div>
   )
--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import http from 'http';
 import { Server  as SocketServer} from 'socket.io';
 
-
 const app = express()
 const server = http.createServer(app);
 const io = new SocketServer(server, {
@@ -11,15 +10,26 @@ const io = new SocketServer(server, {
     }
 });
 
+let messages = [];
+let users = {};
+
 io.on('connection', socket =>{
     console.log(socket.id);
 
-    socket.on('message', (body) =>{
-        console.log(body);
-        socket.broadcast.emit('message', {
-            body,
-            from: socket.id.slice(6)
-        })
+    socket.on('username', (username) => {
+        users[socket.id] = username;
+        socket.emit('messages', messages);
+    })
+
+    socket.on('message', (message) =>{
+        console.log(message.body);
+        // message.from = users[socket.id] || socket.id.slice(6);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+    })
+
+    socket.on('typing', (username) => {
+        socket.broadcast.emit('typing', username);
     })
 })
 
