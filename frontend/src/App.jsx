b837ae1e412b03ac132a4d85b85eb4c84c5b2db4
Milanesa <diegojara88574@gmail.com>
2023-11-08 11:49:01 -0300

    Feat: Chat funcional

---
+++ frontend/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react'
+import io from 'socket.io-client'
+
+const socket = io("/")
+
+function App() {
+
+  const [message, setMessage] = useState("")
+  const [messages, setMessages] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newMessage = {
+      body:message,
+      from: 'Yo'
+    }
+    setMessages([...messages, newMessage])
+    socket.emit('message', message)
+  };
+
+  useEffect (() => {
+    socket.on('message', receiveMessage);
+
+    return () => {
+      socket.off('message', receiveMessage);
+
+    }
+  }, []);
+
+  const receiveMessage = message => 
+  setMessages((state) => [...state, message]);
+
+  return (
+    <div className ="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1 className="text-2xl font-bold my-2"> Chat </h1>
+        <input type="text" 
+        placeholder="Escriba su mensaje"
+        className="border-2 border-zinc-500 p-2 w-full text-black"
+        onChange={(e) => setMessage(e.target.value)}
+        />
+
+        <ul>
+       {
+        messages.map((message, i )=>(
+          <li key = {i} className= {`my-2 p-2 table rounded-md ${message.from === 'Yo' ? 'bg-purple-500'  : 'bg-zinc-700 ml-auto'}`}>
+            <span className="text-xs block">{message.from}:</span>
+        {message.body}
+            </li>
+        ))
+       }
+      </ul>
+
+      </form>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import http from 'http';
+import { Server  as SocketServer} from 'socket.io';
+
+
+const app = express()
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+    }
+});
+
+io.on('connection', socket =>{
+    console.log(socket.id);
+
+    socket.on('message', (body) =>{
+        console.log(body);
+        socket.broadcast.emit('message', {
+            body,
+            from: socket.id.slice(6)
+        })
+    })
+})
+
+server.listen(3000)
+console.log('server on port', 3000);
